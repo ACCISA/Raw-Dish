dd18ba895fef33cf05fc802c64b5a9f0c18250b3
AC <39713625+R0MULLUS@users.noreply.github.com>
2023-01-28 17:45:35 -0500

    Window Class Finish

--- src/main/java/com/example/mchacks/MenuController.java
+++ src/main/java/com/example/mchacks/MenuController.java
@@ -1,16 +1,32 @@
 package com.example.mchacks;
 
+import com.jfoenix.controls.JFXButton;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
-public class MenuController {
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.ResourceBundle;
+
+public class MenuController implements Initializable {
     @FXML
     private Label welcomeText;
+
+    @FXML
+    private JFXButton letUsChoose;
+
 
 
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        byte[] emojiByteCode = new byte[]{(byte)0xF0, (byte)0x9F, (byte)0x98, (byte)0x81};
+        String emoji = new String(emojiByteCode, Charset.forName("UTF-8"));
     }
 }
