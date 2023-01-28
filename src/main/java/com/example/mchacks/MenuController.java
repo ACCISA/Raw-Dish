a49bac4d3f09c0b99cbe9b9be120cb23b6e13b2b
StoneDagher <115055489+Andrew-Dagher@users.noreply.github.com>
2023-01-28 17:55:15 -0500

    new page button

--- src/main/java/com/example/mchacks/MenuController.java
+++ src/main/java/com/example/mchacks/MenuController.java
@@ -1,6 +1,8 @@
 package com.example.mchacks;
 
+import com.example.mchacks.Entities.Window;
 import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
@@ -28,5 +30,11 @@ public class MenuController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         byte[] emojiByteCode = new byte[]{(byte)0xF0, (byte)0x9F, (byte)0x98, (byte)0x81};
         String emoji = new String(emojiByteCode, Charset.forName("UTF-8"));
+    }
+
+    public void onLetUsChoose(ActionEvent actionEvent) {
+        letUsChoose.getScene().getWindow().hide();
+        Window aimenu = new Window("src/main/resources/com/example/mchacks/ai-menu.fxml","src/main/resources/com/example/mchacks/Styles/ai_style.css");
+        aimenu.Open();
     }
 }
