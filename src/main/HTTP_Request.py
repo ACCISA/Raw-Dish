0cad121ee1db68d72c24b89c0795c10d03c5e32d
AC <39713625+R0MULLUS@users.noreply.github.com>
2023-01-29 10:36:09 -0500

    Manual Finish

--- src/main/HTTP_Request.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-params = {'field1': '','field2':'', 'field3':'','field4':'','field5':'','field6':'','field7':'','field8':''}
-response = requests.get('https://pythonexamples.org/', #insert url instead of this one
-            params=params)
-print(response.url)
