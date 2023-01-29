1b45697107425a7f7e9c7ae4394063c1278a195b
richardbadir <116977955+richardbadir@users.noreply.github.com>
2023-01-29 11:07:24 -0500

    Add files via upload

--- src/main/Back-end/data.py
+++ src/main/Back-end/data.py
@@ -1,63 +1,64 @@
 
 def convertData(dataList):
-    returnData = []
+
+    print(dataList[0])
     for i in range(len(dataList)):
         if i == 0: # how has your day been today
             if dataList[i] == "Man":
-                returnData[i] = 0
+                dataList[i] = 0
                 continue
-            if dataList[i] == "Woman":
-                returnData[i] = 1
+            elif dataList[i] == "Woman":
+                dataList[i] = 1
                 continue
         if i == 1:
             if dataList[i] == "Good":
-                returnData[i] = 0
+                dataList[i] = 0
                 continue
             if dataList[i] == "Bad":
-                returnData[i] = 1
+                dataList[i] = 1
                 continue
         if i == 2:
             if dataList[i] == "Under 20$/person":
-                returnData[i] = 0
+                dataList[i] = 0
                 continue
             if dataList[i] == "Over 20$":
-                returnData[i] = 1
+                dataList[i] = 1
                 continue
         if i == 3:
             if dataList[i] == "Very":
-                returnData[i] = 0
+                dataList[i] = 0
                 continue
             if dataList[i] == "A little":
-                returnData[i] = 1
+                dataList[i] = 1
                 continue
         if i == 4 or i == 6:
             if dataList[i] == "Yes":
-                returnData[i] = 0
+                dataList[i] = 0
                 continue
             if dataList[i] == "No":
-                returnData[i] = 1
+                dataList[i] = 1
                 continue
         if i == 5:
             if dataList[i] == "35 or under":
-                returnData[i] = 0
+                dataList[i] = 0
                 continue
             if dataList[i] == "Over 35":
-                returnData[i] = 1
+                dataList[i] = 1
                 continue
 
         if i == 7:
             if dataList[i] == "Alone":
-                returnData[i] = 0
+                dataList[i] = 0
                 continue
             if dataList[i] == "Friends":
-                returnData[i] = 1
+                dataList[i] = 1
                 continue
             if dataList[i] == "Family":
-                returnData[i] = 2
+                dataList[i] = 2
                 continue
             if dataList[i] == "Lover":
-                returnData[i] = 3
+                dataList[i] = 3
                 continue
-    return returnData
+    return dataList
 
             
